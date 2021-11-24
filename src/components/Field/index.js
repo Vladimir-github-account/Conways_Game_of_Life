82c0a3a4f73ed215b2211f7aa984c514132ab945
Vladimir <vladimir1829395441@gmail.com>
2021-11-24 22:53:49 +0200

    created Field component added grid values array to state

---
+++ src/components/Field/index.js
@@ -0,0 +1,23 @@
+import React      from 'react';
+import PropTypes  from 'prop-types';
+import styles     from './fieldStyles.module.sass';
+import classNames from 'classnames';
+
+const Field = props => {
+	const { fieldItem, blackField, whiteField } = styles;
+	const { elementNumber, gridValues, clickHandler } = props;
+	const fieldTypeStyles = gridValues[ elementNumber ].isBlack ? blackField : whiteField;
+	const fieldStyles = classNames( fieldTypeStyles, fieldItem );
+	const clickHandlerWithElement = clickHandler( elementNumber );
+	return (
+			<div key={ elementNumber }
+			     className={ fieldStyles }
+			     onClick={ clickHandlerWithElement }>
+				{ elementNumber + 1 }
+			</div>
+	);
+};
+
+Field.propTypes = {};
+
+export default Field;
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import styles               from './gridStyles.module.scss';
+import Field                from '../Field';
+import _                    from 'lodash';
 
 class Grid extends Component {
 	constructor(props) {
 		super( props );
-		this.state = {};
+		this.state = {
+			gridValues: Array.from( { length: 256 }, (element, index) => {
+				return {
+					isBlack: true,
+					index,
+				};
+			} )
+		};
 	};
 
-	renderGrid(props) {
-		const { gridContainer, gridItem } = styles;
+	clickHandler = (elementNumber) => {
+		return (e) => {
+			const state = _.clone( this.state );
+			state.gridValues[ elementNumber ].isBlack = !state.gridValues[ elementNumber ].isBlack;
+			this.setState( state );
+		};
+	};
+
+	renderGrid() {
+		const { gridContainer } = styles;
 		const getFieldsArray = (rows, cols) => {
 			let content = [];
 			for (let i = 0; i < rows * cols; i++) {
 				content.push(
-						<div key={ i }
-						     className={ gridItem }>{ i + 1 }</div>
+						<Field elementNumber={ i }
+						       gridValues={ this.state.gridValues }
+						       clickHandler={ this.clickHandler }/>
 				);
 			}
 			return content;
